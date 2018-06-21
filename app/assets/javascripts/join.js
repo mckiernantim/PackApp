aab620291edcd52d91a64a25a3936c4ac4ad7c5e
Sharon Morato <sharonmorato1@gmail.com>
2018-06-21 16:29:31 -0400

    toggle active on join

--- app/assets/javascripts/join.js
+++ app/assets/javascripts/join.js
@@ -2,30 +2,12 @@
 // All this logic will automatically be available in application.js.
 
 $(document).ready(function() {
-
+  if (document.querySelector('body.join.index') == undefined) {
+    return;
+  }
   $('.user-auth-toggle').click(function() {
     $('.auth-box').toggleClass('hide');
+    $('.user-auth-toggle').toggleClass('active');
   });
-
-  // $("#login-box-link").click(function() {
-  //   $("#login-box").removeClass('hide');
-  //   $("#signup-box").addClass('hide');
-  // })
-  //
-  // $("#signup-box-link").click(function() {
-  //   $("#signup-box").removeClass('hide');
-  //   $("#login-box").addClass('hide');
-  //
-  // })
-  // $("#login-box-link").click(function() {
-  //  $("#log-in-box").show();
-  //   // $("#login-box-link").removeClass("active");
-  //   // $("#signup-box-link").addClass("active");
-  // });
-  // $("#signup-box-link").click(function() {
-  //   $("#sign-up-box").show();
-  //   $("#log-in-box").hide();
-  // $("#login-box-link").addClass("active");
-  // $("#signup-box-link").removeClass("active");
 
 });
