a5fc156c61e2966aca588ee3a8db5320cce3c5c7
Sharon Morato <sharonmorato1@gmail.com>
2018-06-21 10:59:13 -0400

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # devise_for :users
-  # root to: "home#index"
+  devise_for :users, controllers: {
+       registrations: 'users/registrations'
+     }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root to: "landing#index"
+  root to: "landing#index"
 
 
 end
