336ce766eee7be1a1a2b5ea2db88ee423260e27e
Sharon Morato <sharonmorato1@gmail.com>
2018-06-25 14:48:25 -0400

    dash

--- app/controllers/pre_dashboard_controller.rb
+++ app/controllers/pre_dashboard_controller.rb
@@ -1,5 +1,18 @@
 class PreDashboardController < ApplicationController
 
   def index
+    postal_code = params[:postal_code_input]
+    walkers = User.near(postal_code, 1).all
+    @walkers = walkers.length
+
+
+  end
+
+
+
+
+
+  def pre_dashboard_params
+    params.require(:pre_dashboard).permit(:postal_code)
   end
 end
