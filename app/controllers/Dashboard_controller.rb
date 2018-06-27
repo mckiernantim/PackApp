6c4708c427532b2836b51ffc494eb3dd1334809b
mckiernantim <mckiernantim@gmail.com>
2018-06-27 13:20:28 -0400

    fixed time conflicts in displaying walks

--- app/controllers/Dashboard_controller.rb
+++ app/controllers/Dashboard_controller.rb
@@ -5,9 +5,11 @@ class DashboardController < ApplicationController
             @user_id = current_user.id
             @user = User.find(@user_id)
             @user_credit = current_user.credit
+            @restricted_times = WalkRequest.where(walker_id: @user.id)
+            is_restricted = false;
 
             @dog = @user.dogs[0]
-            @available_walks = WalkRequest.where.not(user_id: @user.id) 
+            @available_walks = WalkRequest.where.not(user_id: @user.id).where(walker_id: nil)
             
             # # will need to be changed once active record is working
             @user_walks = WalkRequest.where(walker_id: @user.id).where(completed: nil)
